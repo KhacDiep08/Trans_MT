500f54a541d45c76f418af8aebc7a2825772eb5f
KhacDiep08 <khacdiep2208@gmail.com>
2025-06-05 21:44:32 +0700

    refactor collate fn

--- data/data_collator.py
+++ data/data_collator.py
@@ -5,12 +5,18 @@ class DataCollatorMT:
         self.pad_id = pad_id
 
     def __call__(self, batch):
-        input_ids = pad_sequence([item["input_ids"] for item in batch], batch_first=True, padding_value=self.pad_id)
-        decoder_input_ids = pad_sequence([item["decoder_input_ids"] for item in batch], batch_first=True, padding_value=self.pad_id)
-        labels = pad_sequence([item["labels"] for item in batch], batch_first=True, padding_value=self.pad_id)
+        input_ids = pad_sequence(
+            [item["input_ids"] for item in batch],
+            batch_first=True,
+            padding_value=self.pad_id
+        )
+        labels = pad_sequence(
+            [item["labels"] for item in batch],
+            batch_first=True,
+            padding_value=-100
+        )
 
         return {
             "input_ids": input_ids,
-            "decoder_input_ids": decoder_input_ids,
             "labels": labels
         }
